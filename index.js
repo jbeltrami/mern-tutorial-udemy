cd9f19cd052f9b0d1e7c6e8021dccbc97a982ffa
jbeltrami <joao_beltrami@hotmail.com>
2020-01-07 16:16:54 -0500

    Install passportjs and strategy for user auth

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
+app.get('/auth/google', (req, res) => {
+  res.send('Trying to authenticate');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
