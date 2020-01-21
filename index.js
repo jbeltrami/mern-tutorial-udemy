93764427e911cd93a0ab2f399f6d492cbf433f17
jbeltrami <joao_beltrami@hotmail.com>
2020-01-21 15:33:29 -0500

    Re-factor project file structure to separate concerns

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
-
+require('./services/passport'); // No need to assign to a variable, since nothing is exported on the source file.
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log({ accessToken });
-      console.log({ refreshToken });
-      console.log({ profile });
-      console.log({ done });
-    },
-  ),
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  }),
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app); // Initialize the routes specified in the source file, and pass the express app as argument.
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
