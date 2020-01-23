c182fa43f1b0f7cbfa94bea710cdaa23a0dd1ad0
jbeltrami <joao_beltrami@hotmail.com>
2020-01-23 15:19:08 -0500

    Add mongoose to connect with mLab DB.

---
+++ config/README.md
@@ -0,0 +1,9 @@
+Create a file labeled `keys.js` and within it, export and object with the following key/value pairs:
+
+```
+  {
+    googleClientID:'IDGoesHere',
+    googleClientSecret: 'SecretGoesHere',
+    mongoURI:'DatabaseURIGoesHere',
+  };
+```
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport'); // No need to assign to a variable, since nothing is exported on the source file.
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
 require('./routes/authRoutes')(app); // Initialize the routes specified in the source file, and pass the express app as argument.
